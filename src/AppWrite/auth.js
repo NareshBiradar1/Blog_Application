b5c04fc07187931f7f682ba7aae89ff9a1f79105
NareshBiradar1 <nareshdb555@gmail.com>
2024-06-20 02:46:08 +0530

    Created Auth Service

---
+++ src/AppWrite/auth.js
@@ -0,0 +1,61 @@
+import { Client, Account, ID } from "appwrite";
+import config from "../conf";
+
+class AuthService{
+    client;
+    account;
+
+    constructor(){
+        this.client = new Client()
+        .setEndpoint(config.appWriteUrl) 
+        .setProject(config.appWriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email , password , name}){
+        try{
+            const userAccount = await account.create(ID.unique() , email , password , name);
+            if(userAccount){
+                return this.login(email , password);
+            }
+            else return userAccount
+            
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async login({email , password}){
+        try{
+           return await this.account.createEmailPasswordSession(email,password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch(error){
+            console.log(" error in authservice :: getcurrentuser "+error)
+        }
+        return null;
+    }
+
+    async logout(){
+        try{
+            await this.account.deleteSessions();
+        }
+        catch(error){
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
