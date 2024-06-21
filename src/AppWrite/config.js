183cb168ad921c8d878b7b4fc387d87a65b899a4
NareshBiradar1 <nareshdb555@gmail.com>
2024-06-22 01:00:16 +0530

    updated appwrite database connection

--- src/AppWrite/config.js
+++ src/AppWrite/config.js
@@ -34,6 +34,106 @@ class DataBaseService{
             console.log(error);
         }
     }
+
+    async updatePost(slug , {title , content , featuredImage , status , userId}){
+        try{
+            return await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async deletePost(slug){
+        try{
+            await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+            return true;
+        }
+        catch(error){
+            console.log(error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try{
+            return await this.databases.getDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug
+            )
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try{
+            return await this.databases.listDocuments(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                queries
+            )
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+                conf.appWriteBucketId,
+                fileId
+            )
+            return true;
+        }
+        catch(error){
+            console.log(error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        try{
+            return this.bucket.getFilePreview(
+                conf.appWriteBucketId,
+                fileId
+            )
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    
 }
 
 const dataBaseService = new DataBaseService();
